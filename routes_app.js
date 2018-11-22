3548b077865d5571cdcc244c83a33f2d72a0d9aa
alexeis <alexeis.carrillo@hotmail.com>
2018-11-21 21:27:17 -0400

    Practica 2

--- routes_app.js
+++ routes_app.js
@@ -19,5 +19,19 @@ module.exports = router;
 */
 
 /*
-form(action="/app/imagenes/#{imagen.id}?_method=PUT", method="POST")
+extends ../../layout.jade
+block contenido
+    div(class="col-md-5 remove-float mx-auto big-top-space")
+        form(action="/app/imagenes/#{imagen.id}?_method=PUT", method="POST")
+
+            h1 Subir nueva imagen
+            div(class="form-group")
+                label(for="nombre") Titulo
+                input(type="text", name="nombre", placeholder="Titulo de la Imagen...", id="nombre", class="form-control",
+                value="#{usuario.nombre}")
+
+            div(class="row top-space")
+                div(class="col-xs-12 col-sm-6")
+                    input(type="submit" value="Actualizar" class="btn btn-info")
+
 */
