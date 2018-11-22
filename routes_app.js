db647fa624f627009db20937f0320b81b05a784c
alexeis <alexeis.carrillo@hotmail.com>
2018-11-21 21:24:32 -0400

    Practica

--- routes_app.js
+++ routes_app.js
@@ -6,10 +6,18 @@ router.route("/usuarios")
 .get(usrCtrl.buscarUsuarios)
 .post(usrCtrl.insertarUsuario)
 
+router.route("/usuarios/:id")
+.get()
+.put()
+.delete()
 module.exports = router;
 /*
     app.post("/usuarios",()=>....)
     app.get("/usuarios", ()=>....)
     app.put("/usuarios", ()=>....)
     app.delete("/usuarios", ()=>....)
+*/
+
+/*
+form(action="/app/imagenes/#{imagen.id}?_method=PUT", method="POST")
 */
